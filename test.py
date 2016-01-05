7d23ac0af3383afef88f2f6e65154ca6638f2bd2
Geng-Yu Laing <liveman24224@gmail.com>
2016-01-05 10:56:01 +0800

    init

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+# a = u"梁耕毓"
+b = "梁耕毓"
+
+
+# print("%s: %d" %(a,len(a)))
+print("%s: %d" %(b,len(b)))
+
+# print(r"%s: %d" %(a.encode('utf8'),len(a.encode('utf8'))))
+# print("%s,%d" %(b.encode('utf8'),len(b.encode('utf8'))))
